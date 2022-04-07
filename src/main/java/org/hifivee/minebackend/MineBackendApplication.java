81bd09c5dc2648b13119b6f619158dfddfc4930f
Komputer-P <wlghks0314@gmail.com>
2022-04-07 21:58:34 +0900

    Spring Boot Web Application Setup

---
+++ src/main/java/org/hifivee/minebackend/MineBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hifivee.minebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hifivee/minebackend/MineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hifivee.minebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
